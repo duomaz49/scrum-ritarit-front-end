1129ff0cceef2aff877ff3a6d5238d71e2ee4ae0
Tomaatti <tuomas49@gmail.com>
2024-11-15 11:33:35 +0200

    Lisätty otsikko

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import TicketSale from "./components/SalesPerson/Sale/TicketSale.tsx";
 function App() {
     return (
         <>
+            <h1>SCRUM RITARIT - Ticket Guru</h1>
             <TicketSale/>
         </>
     )
