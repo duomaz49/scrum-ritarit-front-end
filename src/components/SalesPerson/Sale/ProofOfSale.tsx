ae87b6922147057dcccca6abfaa8e2b26804e51c
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-11-19 22:22:20 +0200

    Lisaa lippuihin qr koodi, vaihda loginin route seka kokeile, jos tulostuksen saisi paremmaksi

--- src/components/SalesPerson/Sale/ProofOfSale.tsx
+++ src/components/SalesPerson/Sale/ProofOfSale.tsx
@@ -3,9 +3,12 @@ import {formatDate, formatTime} from "../../../utils/date.ts";
 import React, {useEffect, useState} from "react";
 import {ISale} from "../../../types/sale.ts";
 import './ProofOfSalePrint.css';
+import { QRCodeSVG } from 'qrcode.react';
+import { IEvent } from '../../../types/event.ts';
 
 interface ProofOfSaleProps {
     sale: ISale;
+    event: IEvent;
     toggleModal?: () => void;
 }
 
@@ -29,8 +32,22 @@ export default function ProofOfSale(props: ProofOfSaleProps) {
     }, []);
 
     return (
-        <Container className="text-center d-flex flex-column align-items-center justify-content-center">
-            <Card className="w-100 m-3 print-overlay">
+        <Container
+            className="text-center d-flex flex-column align-items-center justify-content-center"
+            style={{
+                maxWidth: '1000px',
+                width: '100%',
+                margin: '0 auto',
+            }}
+        >
+            <Card
+                className="wider-card w-100 m-3 print-overlay"
+                style={{
+                    maxWidth: '1000px',
+                    width: '90%',
+                    margin: '0 auto',
+                }}
+            >
                 <CardBody className="text-start">
                     <h5 className="text-center">Transaction - {props.sale.saleId}</h5>
                     <div>Payment method: {props.sale.paymentMethod}</div>
@@ -41,28 +58,36 @@ export default function ProofOfSale(props: ProofOfSaleProps) {
                     <h6 className="text-center">Bought Tickets:</h6>
                     <Table className="mt-2" bordered hover responsive>
                         <thead>
-                        <tr>
-                            <th className="p-2 text-center">Ticket price</th>
-                            <th className="p-2 text-center">Ticket quantity</th>
-                            <th className="p-2 text-center">Ticket number</th>
-                        </tr>
+                            <tr>
+                                <th className="p-2 text-center align-middle">Qr code</th>
+                                <th className="p-2 text-center align-middle">Ticket price</th>
+                                <th className="p-2 text-center align-middle">Quantity</th>
+                                <th className="p-2 text-center align-middle">Ticket code</th>
+                            </tr>
                         </thead>
                         {props.sale.tickets?.map((ticket, i) => (
                             <tbody key={i}>
-                            <tr>
-                                <td className="p-2 text-center">{ticket.price}</td>
-                                <td className="p-2 text-center">{ticket.quantity}</td>
-                                <td className="p-2 text-center">{ticket.ticketNumber}</td>
-                            </tr>
+                                <tr>
+                                    <td className="p-2 text-center">
+                                        <QRCodeSVG
+                                            value={ticket.ticketNumber || "defaultValue"}
+                                            size={80}
+                                            level="L"
+                                        />
+                                    </td>
+                                    <td className="p-2 text-center align-middle">{ticket.price}</td>
+                                    <td className="p-2 text-center align-middle">{ticket.quantity}</td>
+                                    <td className="p-2 text-center align-middle">{ticket.ticketNumber}</td>
+                                </tr>
                             </tbody>
                         ))}
                     </Table>
-                    <hr className="my-4"/>
-                    <div className="d-flex justify-content-around mt-4">
-                        <Button className="hide-on-print" color="danger" onClick={props.toggleModal}>
+                    <hr className="my-4" />
+                    <div className="d-flex justify-content-around mt-4 hide-on-print">
+                        <Button color="danger" onClick={props.toggleModal}>
                             Back
                         </Button>
-                        <Button className="hide-on-print" color="success" onClick={handlePrint}>
+                        <Button color="success" onClick={handlePrint}>
                             Print
                         </Button>
                     </div>
