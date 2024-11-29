4f354ed64ff08bcb2aaa664e1e1bc46d40629c51
Miska Poyry <bhd692@myy.haaga-helia.fi>
2024-11-30 00:39:38 +0200

    Lisaa vahingossa poistettu useNagivate

--- src/components/SalesPerson/SalesPersonView.tsx
+++ src/components/SalesPerson/SalesPersonView.tsx
@@ -3,11 +3,13 @@ import {useNavigate} from "react-router-dom";
 
 export default function SalesPersonView() {
 
+    const navigate = useNavigate();
+
     return (
         <Container className="d-flex justify-content-center">
             <Card className='w-50 m-3 p-4'>
                 <CardBody className='text-start'>
-                    <h1 className="text-center">SalesPerson View</h1>
+                    <h1 className="text-center">Salesperson View</h1>
                     <hr className="my-4"/>
                     <Button color="success" block className="mb-2" onClick={() => navigate('/sales')}>Sell Tickets</Button>
                     <Button color="success" block className="mb-2" onClick={() => navigate('/check')}>Check Tickets</Button>
